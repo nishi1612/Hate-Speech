df2253abc3551d4a8e8b2966df210d47378affed
Smit Shah <smirk14@gmail.com>
2018-10-05 04:07:54 +0530

    Perfected the TF-IDF and started working on POS

--- hate_speech.py
+++ hate_speech.py
@@ -8,34 +8,30 @@ from nltk.stem.wordnet import WordNetLemmatizer
 import re
 import html
 import nltk
+import nltk.sentiment.vader as vader
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import *
 
 hate_speech_data = pd.read_csv('labeled_data.csv')
 
 stopwords = nltk.corpus.stopwords.words("english")
 stopwords.extend('rt')
-stopwords.extend('t')
 
 lemmatizer = WordNetLemmatizer()
-
 def preprocess(tweets):
 	t = html.unescape(tweets)
 	z = lambda x: re.compile('\#').sub('', re.compile('RT @').sub('@', x).strip())
 	t = z(t)
 	tweet = ' '.join(re.sub("(@[_A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",t).split())
 	return tweet
-	#tweet = []
-	#for t in tweets:
-	#	t = html.unescape(t)
-	#	z = lambda x: re.compile('\#').sub('', re.compile('RT @').sub('@', x).strip())
-	#	t = z(t)
-	#	tweet.append(' '.join(re.sub("(@[_A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",t).split()))
 
 
 def tokenizer(tweets):
 	tweet = " ".join(re.split("[^a-zA-Z.,!?]*", tweets.lower())).strip()
 	tweets = [lemmatizer.lemmatize(tweet) for tweet in tweets.split()]
 	return tweets
+
+
 
 vectorizer = TfidfVectorizer(
 	tokenizer = tokenizer,
@@ -49,3 +45,21 @@ vectorizer = TfidfVectorizer(
 	)
 
 tweet = vectorizer.fit_transform(hate_speech_data['tweet']).toarray()
+
+def pos(tokenized_tweet):
+	return nltk.pos_tag(tokenized_tweet)
+
+def pos_tweet(tweet):
+	total_pos_tags = []
+	count = 0
+	for t in tweet:
+		t = preprocess(t)
+		t = tokenizer(t)
+		pos_dict = pos(t)
+		pos_tags = pos_dict[1]
+		pos_tags = " ".join(pos_tags)
+
+		total_pos_tags.append(pos_tags)
+		print(total_pos_tags)
+
+pos_tweet(hate_speech_data['tweet'])
