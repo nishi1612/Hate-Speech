f0360473863cbaa8c3e9dd07cddfa3aced21b6ca
nishi1612 <doshinishi1612@gmail.com>
2018-10-07 13:13:38 +0530

    Update 3
    Functions for finding average syllables per word

--- hate_speech.py
+++ hate_speech.py
@@ -97,4 +97,40 @@ def avg_sentence_length(self, text):
 	words = word_count(text)
 	sentences = sentence_count(text)
 	average_sentence_length = float(words / sentences)
-	return average_sentence_length 
+	return average_sentence_length
+
+# Textstat is a python package, to calculate statistics from
+# text to determine readability,
+# complexity and grade level of a particular corpus.
+# Package can be found at https://pypi.python.org/pypi/textstat
+def syllables_count(word):
+	return textstatistics().syllable_count(word)
+
+# Returns the average number of syllables per
+# word in the text
+def avg_syllables_per_word(text):
+	syllable = syllables_count(text)
+	words = word_count(text)
+	ASPW = float(syllable) / float(words)
+	return legacy_round(ASPW, 1)
+
+# Return total Difficult Words in a text
+def difficult_words(text):
+
+    # Find all words in the text
+	words = []
+	#sentences = break_sentences(text)
+	for sentence in sentences:
+		words += [token for token in sentence]
+
+	# difficult words are those with syllables >= 2
+	# easy_word_set is provide by Textstat as
+	# a list of common words
+	diff_words_set = set()
+
+	for word in words:
+		syllable_count = syllables_count(word)
+		if word not in easy_word_set and syllable_count >= 2:
+			diff_words_set.add(word)
+
+	return len(diff_words_set)
